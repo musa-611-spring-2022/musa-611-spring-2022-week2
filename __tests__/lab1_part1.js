12991ed16eb5bde34742622733f1d343e47a25e1
Mjumbe Poe <mjumbewu@gmail.com>
2022-01-23 19:15:58 -0500

    Initial commit, with lab1

---
+++ __tests__/lab1_part1.js
@@ -0,0 +1,41 @@
+/* global describe, beforeAll, it, page, expect */
+
+beforeAll(async () => {
+  await page.goto('http://localhost:8000/lab/lab1/part1-types-variables-math/');
+});
+
+describe('The variable `a`', () => {
+  it('should have a value greater than 30', async () => {
+    /* global resultTask1 */
+    const taskResult = await page.evaluate(() => resultTask1);
+    expect(taskResult).toBe(true);
+  });
+});
+describe('The variable `b`', () => {
+  it('should have a type of "string"', async () => {
+    /* global resultTask2 */
+    const taskResult = await page.evaluate(() => resultTask2);
+    expect(taskResult).toBe(true);
+  });
+});
+describe('The variable `c`', () => {
+  it('should match the index of the item "plum"', async () => {
+    /* global resultTask3 */
+    const taskResult = await page.evaluate(() => resultTask3);
+    expect(taskResult).toBe(true);
+  });
+});
+describe('The variable `d`', () => {
+  it('should be the length of the string "cassiopeia"', async () => {
+    /* global resultTask4 */
+    const taskResult = await page.evaluate(() => resultTask4);
+    expect(taskResult).toBe(true);
+  });
+});
+describe('The variable `e`', () => {
+  it('should have a remainder of 3 when divided by 5', async () => {
+    /* global resultTask5 */
+    const taskResult = await page.evaluate(() => resultTask5);
+    expect(taskResult).toBe(true);
+  });
+});
---
+++ lab/lab1/part1-types-variables-math/index.js
@@ -0,0 +1,57 @@
+/* =====================
+
+# Lab 1, Part 1 — Types, Variables, Math
+
+## Introduction
+
+Change variables a through e below so that resultTask1 through resultTask5
+evaluate as true. When you are finished, check your browser's console
+log to test the results.
+
+===================== */
+
+let a;
+const resultTask1 = (a > 30);
+
+let b;
+const resultTask2 = (typeof b === 'string');
+
+let c;
+const dataTask3 = ['peach', 'plum', 'pear'];
+const resultTask3 = (dataTask3[c] === 'plum');
+
+let d;
+const resultTask4 = (d === 'cassiopeia'.length);
+
+let e = 18;
+const resultTask5 = (e % 5 === 3);
+
+/* =====================
+
+Tests (all should report success)
+
+===================== */
+
+const taskResults = [
+  resultTask1,
+  resultTask2,
+  resultTask3,
+  resultTask4,
+  resultTask5,
+];
+function reportTaskSuccess(taskNumber) {
+  const taskIdx = taskNumber - 1;
+  const taskSuccess = taskResults[taskIdx];
+
+  if (taskSuccess) {
+    console.log(`Task ${taskNumber} success!`);
+  } else {
+    console.log(`Task ${taskNumber} failure!`);
+  }
+}
+
+reportTaskSuccess(1);
+reportTaskSuccess(2);
+reportTaskSuccess(3);
+reportTaskSuccess(4);
+reportTaskSuccess(5);
