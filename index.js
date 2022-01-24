7501996fefe057745899b37b7129f7f896c8c28a
Anran Zheng <85720119+Anran0716@users.noreply.github.com>
2022-01-24 17:21:16 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,57 +1,140 @@
 /* =====================
 
-# Exercise: Types, Variables, Math
+# Excercise: Abstracting with Functions
 
 ## Introduction
 
-Change variables a through e below so that resultTask1 through resultTask5
-evaluate as true. When you are finished, check your browser's console
-log to test the results.
+In this exercise you will be writing and refactoring code to show data on
+multiple maps on the same page.
+
+
+## Task 1: Change the marker location to your favorite place to eat food in Philadelphia
+
+Take a look at the code below. The part of the code that adds a marker looks
+like this:
+
+    L.marker([39.9522, -75.1639]).bindTooltip('My Location').addTo(foodMap);
+
+The 39.9522, -75.1639 part refers to a set of coordinates: latitude and
+longitude (lat, lng). The 'My Location' part represents the text that will be
+shown when you hover over the marker (this is called a tooltip in leaflet).
+
+In our class, we'll learn a number of ways to find coordinates for a location
+that we want to map. An easy way to do this for a single address is to use
+Google Maps. Check out these instructions to Get the coordinates of a place:
+https://support.google.com/maps/answer/18539?hl=en
+
+Find the coordinates of your favorite place to eat in Philadelphia and update
+the marker coordinates to that place. Change the text in the tooltip to the name
+of that place.
+
+
+## Task 2: Add two more markers
+
+Add two additional markers to your map. These two markers should be two
+additional places where you like to eat food.
+
+To add an additional marker, copy the code you used to create the first marker
+onto the next line. Change the coordinates for your new markers. When you are
+finished, save your file and reload the browser. You should see three markers.
+
+
+## Task 3: Abstract out repeated code
+
+Refactor the code from the previous steps to leverage some of the Javascript
+functionality we have been learning about. We will start by reducing the number
+of times we call the L.marker method. We will do this by "abstracting" from the
+concept of a marker with a tooltip to the concept of a place with a name.
+
+Create a function called "addPlace" that will add a marker with a tooltip to a
+map. Your function should take three arguments in this order:
+
+  1. lat,
+  2. lng,
+  3. name.
+
+Now, instead of using L.marker directly, use your function to add your places to
+the map.
+
+
+## Task 4: Add your favorite places to play in Philadelphia
+
+Notice that there are TWO maps on the page -- one for favorite places to eat and
+the other for favorite places to play. We can use the same function to add
+places to both maps, but first we'll have to update the addPlace function to
+accept an additional argument. Update the function to take the following four
+arguments in this order:
+
+  1. map,
+  2. lat,
+  3. lng,
+  4. name.
+
+Now use the addPlace function to add your three favorite places to play
+(whatever that means to you) in Philadelphia.
+
+## Stretch Task: Use custom icons
+
+Using the Leaflet documentation (https://leafletjs.com/), try to set up your
+maps to use custom icons on your markers.
 
 ===================== */
 
-let a = 31;
-const resultTask1 = (a > 30);
+const foodMap = L.map('food-map', {
+  center: [39.9522, -75.1639],
+  zoom: 14,
+});
 
-let b = 'Kelly';
-const resultTask2 = (typeof b === 'string');
+L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
+  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  subdomains: 'abcd',
+  minZoom: 0,
+  maxZoom: 20,
+  ext: 'png',
+}).addTo(foodMap);
 
-let c = 1;
-const dataTask3 = ['peach', 'plum', 'pear'];
-const resultTask3 = (dataTask3[c] === 'plum');
+function addPlace(lat,lng,name)
+{
+  L.marker([lat, lng]).bindTooltip(name).addTo(foodMap);
+}
 
-let d = 10;
-const resultTask4 = (d === 'cassiopeia'.length);
+const playMap = L.map('play-map', {
+  center: [39.9522, -75.1639],
+  zoom: 14,
+});
 
-let e = 8;
-const resultTask5 = (e % 5 === 3);
+L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
+  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  subdomains: 'abcd',
+  minZoom: 0,
+  maxZoom: 20,
+  ext: 'png',
+}).addTo(playMap);
+
+function addPlace(lat, lng, name, map)
+{
+  L.marker([lat, lng]).bindTooltip(name).addTo(map);
+}
 
 /* =====================
 
-Tests (all should report success)
+Start code
 
 ===================== */
 
-const taskResults = [
-  resultTask1,
-  resultTask2,
-  resultTask3,
-  resultTask4,
-  resultTask5,
-];
-function reportTaskSuccess(taskNumber) {
-  const taskIdx = taskNumber - 1;
-  const taskSuccess = taskResults[taskIdx];
+addPlace(39.952395181787665, -75.17204718870082, 'El Rey', foodMap);
+addPlace(39.952354, -75.168764, 'Green Eggs Caffee', foodMap);
+addPlace(39.955295, -75.170517, 'The Mulberry', foodMap);
+/*
+L.marker([39.952395181787665, -75.17204718870082]).bindTooltip('El Rey').addTo(foodMap);
+L.marker([39.952354, -75.168764]).bindTooltip('Green Eggs Caffee').addTo(foodMap);
+L.marker([39.955295, -75.170517]).bindTooltip('The Mulberry').addTo(foodMap);
+*/
+addPlace(39.954078, -75.165653, 'Love Park', playMap);
+addPlace(39.952298, -75.163886, 'City hall', playMap);
+addPlace(39.952206, -75.179706, '2400 chestnut', playMap);
+/* =====================
 
-  if (taskSuccess) {
-    console.log(`Task ${taskNumber} success!`);
-  } else {
-    console.log(`Task ${taskNumber} failure!`);
-  }
-}
+End code
 
-reportTaskSuccess(1);
-reportTaskSuccess(2);
-reportTaskSuccess(3);
-reportTaskSuccess(4);
-reportTaskSuccess(5);
+===================== */
