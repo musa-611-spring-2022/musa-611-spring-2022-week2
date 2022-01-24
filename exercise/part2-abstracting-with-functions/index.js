b73712a2d933aa9f220017d393169b635217da52
Hanpu Yao <81696320+yhp92@users.noreply.github.com>
2022-01-24 16:43:27 -0500

    Add files via upload

--- exercise/part2-abstracting-with-functions/index.js
+++ exercise/part2-abstracting-with-functions/index.js
@@ -57,6 +57,7 @@ Now, instead of using L.marker directly, use your function to add your places to
 the map.
 
 
+
 ## Task 4: Add your favorite places to play in Philadelphia
 
 Notice that there are TWO maps on the page -- one for favorite places to eat and
@@ -111,8 +112,28 @@ L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ex
 Start code
 
 ===================== */
+// task 1
+L.marker([39.953392, -75.187945]).bindTooltip('My Apartment').addTo(foodMap);
 
-L.marker([39.9522, -75.1639]).bindTooltip('My Location').addTo(foodMap);
+// task 2
+L.marker([39.95309042323159, -75.19053510642438]).bindTooltip('Hill College House').addTo(foodMap);
+L.marker([39.95549395545588, -75.15801782243038]).bindTooltip('Chinatown').addTo(foodMap);
+
+// task 3
+function addPlace(lat,lng,name){
+  L.marker([lat, lng]).bindTooltip(name).addTo(foodMap);
+}
+
+addPlace(playMap,39.953090423255159, -75.19053510642558, "IDK");
+
+
+// task 4
+
+function addPlace(map, lat,lng,name){
+  L.marker([lat, lng]).bindTooltip(name).addTo(map);
+}
+
+addPlace(playMap,39.953090422324,-75.158017899999, "IDK");
 
 /* =====================
 
