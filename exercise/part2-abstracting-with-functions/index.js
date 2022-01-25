70d0577bc6ed0885b7198bb505de223de1326f02
hlechuan <mnxanlh@gmail.com>
2022-01-24 19:17:47 -0500

    Update index.js

--- exercise/part2-abstracting-with-functions/index.js
+++ exercise/part2-abstracting-with-functions/index.js
@@ -117,7 +117,7 @@ L.marker([39.953154, -75.156851]).bindTooltip('Happy Lamb').addTo(foodMap);
 L.marker([39.953604, -75.175432]).bindTooltip('Double Knot').addTo(foodMap);
 L.marker([39.949683, -75.162188]).bindTooltip('Pho Street').addTo(foodMap);
 
-function addPlace(lat, lon, name) {
+function addPlace3(lat, lon, name) {
   return L.marker([lat, lon]).bindTooltip(name).addTo(foodMap);
 }
 
