9d3afeac1ca19acd805934689afcd9cea8acf11c
bri-ne <cervantesblb@gmail.com>
2022-01-31 12:56:36 -0500

    edit#2

--- exercise/part2-abstracting-with-functions/index.js
+++ exercise/part2-abstracting-with-functions/index.js
@@ -111,22 +111,16 @@ L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ex
 Start code
 
 ===================== */
-
-/* ### 1. & 2. 
-L.marker([39.93946748830836, -75.15588034546359]).bindTooltip('Lazeez Indian Cuisine').addTo(foodMap);
-L.marker([39.94891061831568, -75.15998250289982]).bindTooltip('Xiandu Thai Fusion Cuisine').addTo(foodMap);
-L.marker([39.949695323213234, -75.153255972355]).bindTooltip('Morimoto Sushi').addTo(foodMap);
-*/
 /* ### 3. & 4. */
-const addPlace = (myLat, myLon, placeName, mapName) => {
+const addPlace = (map, lat, lon, name) => {
   L.marker([myLat, myLon]).bindTooltip(placeName).addTo(mapName);
 };
-addPlace(39.93946748830836, -75.15588034546359, 'Lazeez Indian Cuisine', foodMap);
-addPlace(39.94891061831568, -75.15998250289982, 'Xiandu Thai Fusion Cuisine', foodMap);
-addPlace(39.949695323213234, -75.153255972355, 'Morimoto Sushi', foodMap);
-addPlace(39.94222347556279, -75.15054476208091, 'Philly Game Shop', playMap);
-addPlace(39.94671778533936, -75.15231720668906, 'Washington Square Park', playMap);
-addPlace(39.94535695388852, -75.16275726603352, 'Dirty Franks', playMap);
+addPlace(foodMap, 39.93946748830836, -75.15588034546359, 'Lazeez Indian Cuisine');
+addPlace(foodMap, 39.94891061831568, -75.15998250289982, 'Xiandu Thai Fusion Cuisine');
+addPlace(foodMap, 39.949695323213234, -75.153255972355, 'Morimoto Sushi');
+addPlace(playMap, 39.94222347556279, -75.15054476208091, 'Philly Game Shop');
+addPlace(playMap, 39.94671778533936, -75.15231720668906, 'Washington Square Park');
+addPlace(playMap, 39.94535695388852, -75.16275726603352, 'Dirty Franks');
 /* =====================
 
 End code
